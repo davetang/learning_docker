c53011f594e7ebb0b2fd438d0f988319f84943a9
davetang <davetingpongtang@gmail.com>
2020-10-26 11:29:57 +0900

    Directory permission

--- rstudio/run_docker.sh
+++ rstudio/run_docker.sh
@@ -1,15 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ver=$(cat Dockerfile | grep "^FROM" | cut -f2 -d':')
 
 rstudio_image=davetang/rstudio:${ver}
+container_name=rstudio_dtang
+port=8888
+
+>&2 echo $container_name listening on port $port
 
 docker run --rm \
+           -p $port:8787 \
            -d \
-           -p 8888:8787 \
-           -v ~/github/learning_docker/rstudio/packages:/packages \
-           -v ~/github/learning_docker/rstudio/notebooks:/notebooks \
+           --name $container_name \
+           -v ~/packages:/packages \
            -v ~/github/learning_docker/rstudio:/data \
            -e PASSWORD=password \
+           -e USERID=$(id -u) \
+           -e GROUPID=$(id -g) \
            $rstudio_image
 
