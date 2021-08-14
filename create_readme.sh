b7f3515d648f6f49116248741d383bc08a6b71b3
davetang <davetingpongtang@gmail.com>
2021-08-14 21:26:03 +0900

    Create README using R Markdown

---
+++ create_readme.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if ! [ -x "$(command -v gh-md-toc)" ]; then
+   >&2 echo Please install gh-md-toc from https://github.com/ekalinin/github-markdown-toc
+   exit 1
+fi
+
+if ! [ -x "$(command -v Rscript)" ]; then
+   >&2 echo Could not find Rscript. Please make sure R is installed.
+   exit 1
+fi
+
+out_md=tmp.md
+Rscript -e "rmarkdown::render('readme.Rmd', output_file=\"$out_md\")"
+gh-md-toc $out_md > toc
+
+cat toc <(echo) <(date) <(echo) $out_md > README.md
+
+rm $out_md toc
+
+>&2 echo Done!
+
+exit 0
+
