bd7027d3c0fa7d56c697d92992173c16f9e9fe5b
davetang <davetingpongtang@gmail.com>
2022-04-08 22:38:28 +0900

    R-4.1.3

--- r/build.sh
+++ r/build.sh
@@ -7,6 +7,8 @@ image=r_build
 
 docker build -t davetang/${image}:${ver} .
 
-# docker login
-# docker push davetang/${image}:${ver}
+>&2 echo Build complete
+>&2 echo -e "Run the following to push to Docker Hub:\n"
+>&2 echo docker login
+>&2 echo docker push davetang/${image}:${ver}
 
