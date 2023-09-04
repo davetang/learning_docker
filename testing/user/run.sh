5cae7e2b0503c8d1d71e187428d6f7e851ebd16a
Dave Tang <davetingpongtang@gmail.com>
2023-09-04 13:47:09 +0900

    Install gosu

--- testing/user/run.sh
+++ testing/user/run.sh
@@ -2,13 +2,12 @@
 
 set -euo pipefail
 
-image=user
+image=usertest
 ver=22.10
 
 docker build -t davetang/${image}:${ver} .
 
-docker run --rm davetang/${image}:${ver}
-
-docker rmi davetang/${image}:${ver} > /dev/null 2> /dev/null
+# docker run --rm davetang/${image}:${ver}
+# docker rmi davetang/${image}:${ver} > /dev/null 2> /dev/null
 
 exit 0
