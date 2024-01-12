e88a09f1d807ea25030298f083ebb4d0afad5a48
Dave Tang <davetingpongtang@gmail.com>
2024-01-12 09:45:06 +0900

    0.0.3

--- mkdocs/build.sh
+++ mkdocs/build.sh
@@ -2,10 +2,9 @@
 
 set -euo pipefail
 
-ver=0.0.2
+ver=0.0.3
 
 docker build -t davetang/mkdocs:${ver} .
 
 # docker login
 # docker push davetang/mkdocs:${ver}
-
--- mkdocs/mkdocs_serve.sh
+++ mkdocs/mkdocs_serve.sh
@@ -2,12 +2,21 @@
 
 set -euo pipefail
 
+# set project/directory name
+proj=test
+
+ver=0.0.3
+docker_image=davetang/mkdocs:${ver}
+container_name=mkdocs_dtang
+port=5555
+
+dir=$(pwd)/${proj}/site
 if [[ ! -x $(command -v docker) ]]; then
    >&2 echo Could not find docker
    exit 1
 fi
 
-proj=test
+check_image=$(docker image inspect ${docker_image})
 
 if [[ ! -d ${proj} ]]; then
    >&2 echo Creating ${proj}
@@ -15,7 +24,7 @@ if [[ ! -d ${proj} ]]; then
      --rm \
      -u $(stat -c "%u:%g" README.md) \
      -v $(pwd):/work \
-     davetang/mkdocs:0.0.1 \
+     ${docker_image} \
      mkdocs new ${proj}
 fi
 
@@ -25,29 +34,22 @@ if [[ ! -d ${proj}/site ]]; then
      --rm \
      -u $(stat -c "%u:%g" README.md) \
      -v $(pwd)/${proj}:/work \
-     davetang/mkdocs:0.0.1 \
+     ${docker_image} \
      mkdocs build
 fi
 
-ver=0.0.1
-docker_image=davetang/mkdocs:${ver}
-check_image=$(docker image inspect ${docker_image})
-container_name=mkdocs_dtang
-port=5555
-dir=$(pwd)/${proj}/site
-
-docker run --rm \
+docker run \
+   --rm \
    -d \
    -p ${port}:${port} \
-   --name $container_name \
+   --name ${container_name} \
    -v ${dir}:/work \
-   $docker_image \
+   ${docker_image} \
    python -m http.server ${port}
 
->&2 echo $container_name listening on port $port
+>&2 echo ${container_name} listening on port $port
 >&2 echo Copy and paste http://localhost:$port into your browser
 >&2 echo To stop container run: docker stop ${container_name}
 >&2 echo Done
 
 exit 0
-
